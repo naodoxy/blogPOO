ba091a758e4c9a9e249ea437e193a266c04b4ce2
naodoxy <sarahlaine864@gmail.com>
2026-01-05 14:26:52 +0100

    factorisation

---
+++ libraries/database.php
@@ -0,0 +1,11 @@
+<?php
+
+function getPdo(){
+
+    $pdo = new PDO('mysql:host=localhost;dbname=blogpoo';charset=utf8', 'root', '',)[
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+]);
+
+    return $pdo;    
+}
